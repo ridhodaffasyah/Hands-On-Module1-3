72fe8ec22f104d05e8f1e197b880d80e57d785d1
ridhodaffasyah <ridhodaffasyah01@gmail.com>
2022-04-20 09:16:28 +0700

    fix

--- hands-on-3-1/src/component/gif/gif.jsx
+++ hands-on-3-1/src/component/gif/gif.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
+
 function Gif({title, url}) {
     return (
         <>
             <div className="image">
-                <h1>{title}</h1>
+                <h1 style={{ fontSize: "14px" }}>{title}</h1>
                 <img src={url} className='img' alt="From Giphy"></img>
             </div>
         </>
